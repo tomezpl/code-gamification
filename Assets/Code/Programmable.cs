6de678536faaa94d4331ac06290ebc5e1bcf2fd2
Tomasz Zając <tomezpl@gmail.com>
2020-01-06 21:25:44 +0000

    Adds base for programmable objects

---
+++ Assets/Code/Programmable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Programmable : MonoBehaviour
+{
+    // Object containing program code to control this Programmable.
+    public GameObject Computer;
+
+    // Element (if the ProgramController addresses an array of objects)
+    public int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Computer.GetComponent<ProgramController>().ExecuteFrame(index, gameObject);
+    }
+}

7a6104d3e100a79acfb2f293c9cfd4952acfa6e4
Tomasz Zając <tomezpl@gmail.com>
2020-02-21 13:29:18 +0000

    CurrentLine is rendered similarly to overlay

---
+++ Assets/Code/TerminalLookAtCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalLookAtCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ImpersonateOverlay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ImpersonateOverlay();
+    }
+
+    void ImpersonateOverlay()
+    {
+        Transform cam = GameObject.Find("Player").GetComponentInChildren<Camera>().transform;
+        transform.eulerAngles = new Vector3(-90.0f + cam.eulerAngles.x, cam.eulerAngles.y, cam.eulerAngles.z);
+    }
+}

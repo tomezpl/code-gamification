15dd12762952b2671e3723e3b60d1ffe488f1cc3
Tomasz Zając <tomezpl@gmail.com>
2020-03-31 20:05:43 +0100

    Eliminates stutters when saving logs

--- Assets/Code/Logger.cs
+++ Assets/Code/Logger.cs
@@ -9,34 +9,62 @@ using UnityEngine;
 public class Logger : MonoBehaviour
 {
     private static bool initialised = false;
+    private static string logBuf = "";
+    private const int logChLimit = 1024; // approximate character limit for the log buffer. At this point, we would write it to disk (as opposed to writing on every log)
+    private static string logFn = "";
 
     private static void init()
     {
         if (!initialised)
         {
-            File.WriteAllText("log.txt", "");
+            logFn = $"log-{DateTime.Now.ToString().Replace(":", ".").Replace("/", "-")}.txt";
+            File.WriteAllText(logFn, "");
         }
         initialised = true;
+    }
+
+    private static void saveAndFlush()
+    {
+        File.AppendAllText(logFn, logBuf);
+        logBuf = "";
     }
 
     public static void Log(string message)
     {
         init();
         Debug.Log(message);
-        File.AppendAllText("log.txt", $"({DateTime.Now.ToString()}) INFO:\n{message}\n");
+        logBuf += $"({DateTime.Now.ToString()}) INFO:\n{message}\n";
+
+        // Flush if log character limit reached or every 30s
+        if (logChLimit <= logBuf.Length || (Mathf.RoundToInt(Time.time) % 30) < 2)
+        {
+            saveAndFlush();
+        }
     }
 
     public static void LogError(string message)
     {
         init();
         Debug.LogError(message);
-        File.AppendAllText("log.txt", $"({DateTime.Now.ToString()}) ERROR:\n{message}\n");
+        logBuf += $"({DateTime.Now.ToString()}) ERROR:\n{message}\n";
+
+        // Flush if log character limit reached or every 30s
+        if (logChLimit <= logBuf.Length || (Mathf.RoundToInt(Time.time) % 30) < 5)
+        {
+            saveAndFlush();
+        }
     }
 
     public static void LogWarning(string message)
     {
         init();
         Debug.LogWarning(message);
-        File.AppendAllText("log.txt", $"({DateTime.Now.ToString()}) WARNING:\n{message}\n");
+        logBuf += $"({DateTime.Now.ToString()}) WARNING:\n{message}\n";
+
+        // Flush if log character limit reached or every 30s
+        if(logChLimit <= logBuf.Length || (Mathf.RoundToInt(Time.time) % 30) < 5)
+        {
+            saveAndFlush();
+        }
     }
 }
